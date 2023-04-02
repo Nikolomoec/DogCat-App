72f370c393dfe33339a1fa801d510154b9b62531
Nikolomoec <nikolomoec@gmail.com>
2023-04-02 19:59:38 +0300

    Initial Commit

---
+++ DogCat App/DogCat_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DogCat_AppApp.swift
+//  DogCat App
+//
+//  Created by Nikita Kolomoec on 02.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct DogCat_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
